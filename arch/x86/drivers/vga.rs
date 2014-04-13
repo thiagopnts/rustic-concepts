0783c9a0487c3cf2aa6af26176cbe26f7172efa8
thiagopnts <thiagopnts@gmail.com>
2014-04-13 03:07:08 -0300

    add Char type

---
+++ arch/x86/drivers/vga.rs
@@ -0,0 +1,52 @@
+use std::num::FromPrimitive;
+
+#[deriving(Show, FromPrimitive)]
+pub enum Color {
+  Black = 0,
+  Blue  = 1,
+  Green = 2,
+  Cyan = 3,
+  Red = 4,
+  Pink = 5,
+  Brown = 6,
+  LightGray = 7,
+  DarkGray = 8,
+  LightBlue = 9,
+  LightGreen = 10,
+  LightCyan = 11,
+  LightRed = 12,
+  LightPink = 13,
+  Yellow = 14,
+  White = 15,
+}
+
+struct Char {
+  pub char: u8,
+  attr: u8, // 4 bits for foreground and 4 bits for background
+}
+
+impl Char {
+  pub fn new(c: char, fg: Color, bg: Color) -> Char {
+    Char { char: c as u8, attr: fg as u8 | (bg as u8 << 4) }
+  }
+
+  pub fn background(&self) -> Color {
+    match FromPrimitive::from_u8(self.attr >> 4 as u8) {
+      Some(color) => color,
+      None => Black
+    }
+  }
+
+  pub fn foreground(&self) -> Color {
+    match FromPrimitive::from_u8(self.attr & 0x0F as u8) {
+      Some(color) => color,
+      None => Black
+    }
+  }
+}
+
+fn main() {
+  let c = Char::new('f', Green, Black);
+  println!("foreground: {}, background: {}", c.foreground(), c.background());
+}
+
